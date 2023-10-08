63e21cf5392ae61ef8be62849127658011c655de
daivadva <daiva.daniuleviciute@gmail.com>
2023-10-08 13:57:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the amount of numbers:");
+        n = scanner.nextInt();
+        int[] array = new int[10];
+        System.out.println("Enter numbers:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("Numbers are:");
+        for (int i = 0; i < n; i++) {
+
+            System.out.println(array[i]);
+
+            if (array[i] != 0) {
+                count++;
+                continue;
+            } else {
+                break;
+            }
+        }
+        System.out.println("Amount of numbers are: " + count);
+        System.out.println("Full array:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+
+        }
+    }
+
+}
